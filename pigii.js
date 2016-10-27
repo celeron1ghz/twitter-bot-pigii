017b089e582c15dab9ef83dd4450ce5d5d766655
celeron1ghz <celeron1ghz@gmail.com>
2016-10-27 19:25:43 +0900

    added lib pigii

---
+++ pigii.js
@@ -0,0 +1,18 @@
+exports.repeat = (str,repeat) => {
+    return Array(repeat + 1).join(str);
+};
+
+exports.random_length_string = (str) => {
+    var repeat;
+    do { repeat = Math.floor(Math.random() * 10) } while ( repeat == 0 );
+    return this.repeat(str, repeat);
+};
+
+exports.random_pigii_string = (str) => {
+    return [
+        'ピギィ',
+        this.random_length_string('ィ'),
+        this.random_length_string('イ'),
+        'ーーーーーーーーーー',
+    ].join('');
+};
