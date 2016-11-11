7bdec428b51b58ff834366fd190fdfe12fbe8538
celeron1ghz <celeron1ghz@gmail.com>
2016-11-11 13:20:36 +0900

    milli-sec precision calc added

--- handler.js
+++ handler.js
@@ -6,9 +6,12 @@ var SECONDS = 0;
 let twitter = require('twitter');
 let sleep   = require('sleep-async')();
 let pigii   = require('./pigii.js');
+require('date-utils');
 
 let config  = require('./config.js');
 let client  = new twitter(config);
+
+Date.toString = () => { return this.toFormat("YYYY-MM-DD HH24:MI:SS") + this.getMilliseconds() };
 
 module.exports.pigii = (event, context, callback) => {
     let now = new Date();
@@ -20,25 +23,28 @@ module.exports.pigii = (event, context, callback) => {
     let diff = targetTime.getTime() - now.getTime();
 
     if (diff >= 300000) {
-        let mess = "Too far to invoke. invoke in 30 sec. sec=" + diff;
+        let mess = new Date() + ": Too far to invoke. invoke in 30 sec. sec=" + diff;
         console.log(mess);
         callback(null, { message: mess });
         return;
     }
 
-    console.log('tweet after ' + diff + ' millisec...');
+    // millisec precision calculating
+    diff = diff - 1000 + (1000 - now.getMilliseconds());
+    console.log(new Date() + ': tweet after ' + diff + ' millisec...');
 
     sleep.sleep(diff, () => {
-        console.log("let's pigii!!!!!");
+        console.log(new Date() + ": let's pigii!!!!!");
 
         client.post('statuses/update', {
             status: pigii.random_pigii_string()
         }, (error, tweet, response) => {
             if (error)  {
+                console.log(new Date() + ": pigii failed. " + error);
                 console.log(error);
                 callback(null, error);
             } else {
-                console.log("Posted a tweet #" + tweet.id + " --> " + tweet.text);
+                console.log(new Date() + ": pigii finished. #" + tweet.id + " --> " + tweet.text);
                 callback(null, { id: tweet.id, tweet: tweet.text });
             }   
         });
