0f2f87e4cb3090775e3b2674845d1830a6399a53
celeron1ghz <celeron1ghz@gmail.com>
2017-09-05 13:38:23 +0900

    using vo and credstash

--- handler.js
+++ handler.js
@@ -3,46 +3,44 @@
 const MINUTES = 0;
 const SECONDS = 0;
 const twitter = require('twitter');
-const pigii   = require('./pigii.js');
-const client  = new twitter({
-    consumer_key:        process.env.PIGII_CONSUMER_KEY,
-    consumer_secret:     process.env.PIGII_CONSUMER_SECRET,
-    access_token_key:    process.env.PIGII_ACCESS_TOKEN_KEY,
-    access_token_secret: process.env.PIGII_ACCESS_TOKEN_SECRET,
-});
+const pigii = require('./pigii.js');
+const cred = require('credstash-promise');
+const vo = require('vo');
 
 module.exports.main = (event, context, callback) => {
-    Promise.resolve()
-        .then(data => {
-            const now = new Date();
-            const targetTime = new Date(now.getTime());
-            targetTime.setHours(targetTime.getHours() + 1);
-            targetTime.setMinutes(MINUTES);
-            targetTime.setSeconds(SECONDS);
-
-            let diff = targetTime.getTime() - now.getTime();
-
-            if (diff >= 300000) {
-                throw Error(`Too far to invoke. invoke in 30 sec. sec=${diff}`);
-            }
-
-            // millisec precision calculating
-            diff = diff - 1000 + (1000 - now.getMilliseconds());
-            console.log('tweet after ' + diff + ' millisec...');
-            return new Promise((resolve,reject) => setTimeout(diff, resolve));
-        })
-        .then(data =>
-            new Promise((resolve,reject) => {
-                console.log("let's pigii!!!!!");
-                return client.post('statuses/update', { status: pigii.random_pigii_string() })
-            })
-        )
-        .then(data => {
-            console.log(`pigii finished. #${data.id} --> ${data.text}`);
-            callback(null, data);
-        })
-        .catch(err => {
-            console.log("error happen:", err);
-            callback(err);
+    vo(function*(){
+        const client  = new twitter({
+            consumer_key:        yield cred.fetchCred('PIGII_CONSUMER_KEY'),
+            consumer_secret:     yield cred.fetchCred('PIGII_CONSUMER_SECRET'),
+            access_token_key:    yield cred.fetchCred('PIGII_ACCESS_TOKEN_KEY'),
+            access_token_secret: yield cred.fetchCred('PIGII_ACCESS_TOKEN_SECRET'),
         });
+
+        const now = new Date();
+        const targetTime = new Date(now.getTime());
+        targetTime.setHours(targetTime.getHours() + 1);
+        targetTime.setMinutes(MINUTES);
+        targetTime.setSeconds(SECONDS);
+
+        let diff = targetTime.getTime() - now.getTime();
+
+        if (diff >= 300000) {
+            throw Error(`Too far to invoke. invoke in 30 sec. sec=${diff}`);
+        }
+
+        // millisec precision calculating
+        diff = diff - 1000 + (1000 - now.getMilliseconds());
+        console.log('tweet after ' + diff + ' millisec...');
+        yield new Promise((resolve,reject) => setTimeout(diff, resolve));
+
+        console.log("let's pigii!!!!!");
+        const data = yield client.post('statuses/update', { status: pigii.random_pigii_string() })
+
+        console.log(`pigii finished. #${data.id} --> ${data.text}`);
+        callback(null, data);
+    })
+    .catch(err => {
+        console.log("error happen:", err);
+        callback(err);
+    });
 };
